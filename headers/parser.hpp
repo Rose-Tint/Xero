a46fcb34b8ff45ee735a0df8086ee7964dee140e
Joe <schubarthjoe7@gmail.com>
2021-09-08 21:31:37 -0500

    Started parsing function

--- headers/parser.hpp
+++ headers/parser.hpp
@@ -1,34 +1,6 @@
 #pragma once
 
-#include <fstream>
-#include <unordered_set>
-#include <stack>
-#include <string>
-#include <sstream>
-#include <vector>
-#include "ast.hpp"
+#include "expression.hpp"
 
 
-class Parser
-{
-    std::stringstream code;
-    std::vector<Expression*> custom_ops;
-    std::stack<Expression*> operand_stk;
-    std::stack<Expression*> operator_stk;
-
-    void add_custom_oper();
-    void process(std::string);
-    void import(std::string);
-    expr_t* parse();
-    expr_t make_number();
-    expr_t make_identifier();
-    oper_t make_operator();
-    bool is_operator(char);
-    bool is_identifier(char);
-    bool is_valid_number_char(char);
-    bool is_operator(char);
-    
-    public:
-    Parser() = default;
-    expr_t* operator()(std::vector<token_t>);
-};
+ExprNode parse();
