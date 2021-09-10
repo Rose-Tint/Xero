1a07335cebc1a41032dae626911d7bb8b8803176
Joe <schubarthjoe7@gmail.com>
2021-09-09 23:50:12 -0500

    Added symbols and operators maps

--- headers/token.hpp
+++ headers/token.hpp
@@ -1,17 +1,50 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
 
 
 enum TokenType
 {
     IDENTIFIER,
     KEYWORD,
+    SYMBOL,
     ENCAP,
     ENDL,
     UNOP,
     BIOP,
+    ASSOP,
     LITERAL
+};
+
+
+const std::unordered_map<char, TokenType> symbols
+{
+    { '{' , ENCAP  }, { '}'  , ENCAP  },
+    { '[' , ENCAP  }, { ']'  , ENCAP  },
+    { ':' , SYMBOL }, { '('  , ENCAP  },
+    { ')' , ENCAP  }, { '"'  , ENCAP  },
+    { ';' , ENDL   }, { '\'' , ENCAP  },
+};
+
+const std::unordered_map<std::string, TokenType> operators
+{
+    { "!"  , UNOP  }, { "@"  , UNOP  },
+    { "#"  , UNOP  }, { "-"  , UNOP  },
+    { "^"  , BIOP  }, { "&"  , BIOP  },
+    { "*"  , BIOP  }, { "%"  , BIOP  },
+    { "+"  , BIOP  }, { "<"  , BIOP  },
+    { ">"  , BIOP  }, { ","  , BIOP  },
+    { "."  , BIOP  }, { "/"  , BIOP  },
+    { "|"  , BIOP  }, { "<=" , BIOP  },
+    { ">=" , BIOP  }, { "!=" , BIOP  },
+    { "++" , BIOP  }, { "--" , BIOP  },
+    { "="  , BIOP  }, { ":=" , ASSOP },
+    { "%=" , ASSOP }, { "^=" , ASSOP },
+    { "*=" , ASSOP }, { "-=" , ASSOP },
+    { "+=" , ASSOP }, { "/=" , ASSOP },
+    { "&=" , ASSOP }, { "|=" , ASSOP },
+    { "::" , ASSOP },
 };
 
 
@@ -20,12 +53,5 @@ struct token_t
     const TokenType type;
     const std::string value;
     bool is_final() const { return (type == IDENTIFIER) || (type == LITERAL); }
-    token_t operator=(const token_t&);
+    token_t operator=(const token_t& other) { return  { other.type, other.value }; }
 };
-
-
-token_t token_t::operator=(const token_t& other)
-{
-    token_t copy { other.type, other.value };
-    return copy;
-}
